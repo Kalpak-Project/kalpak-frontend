b69ed28759096a40c102282ca950f836f564cd3b
alex94chen <alex94chen@gmail.com>
2022-03-21 19:53:23 +0200

    Arrange modal add to enable existing options in DB. Arranging date format

--- src/pages/ManningTable.js
+++ src/pages/ManningTable.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback, useContext } from 'react';
 import axios from 'axios';
-import { Table } from 'antd';
+import { Table , DatePicker } from 'antd';
 import ModalAdd from '../components/ModalAdd';
 import { withUser } from '../components/userContext';
 import { Navigate } from 'react-router-dom';
+import { Role, RoleSelect } from './RolesTable';
+import Moment from 'react-moment';
 
 
 const ManningTable = withUser(({user}) => {
@@ -36,30 +38,35 @@ const ManningTable = withUser(({user}) => {
             title: 'User ID',
             dataIndex: 'User ID',
             width: '25%',
+            render: (text, record) => <Role value={text}/>,
+            inputRender: (value, onChange)=><RoleSelect value={value} onChange={onChange}/>,
         },
         {
             title: 'Role ID',
             dataIndex: 'Role ID',
             width: '25%',
+            render: value => <Role value={value}/>
         },
         {
             title: 'Date of staffing',
             dataIndex: 'Date of staffing',
             width: '25%',
+            render: value => <Moment date={value}/>,
+            inputRender: (value, onChange)=><DatePicker date={value} onChange={(newValue)=>onChange(newValue.toISOString())}/>,
         },
         {
             title: 'Job end date',
             dataIndex: 'Job end date',
             width: '25%',
+            render: value => <Moment date={value}/>,
+            inputRender: (value, onChange)=><DatePicker date={value} onChange={(newValue)=>onChange(newValue.toISOString())}/>,
         }
 
     ]
 
-    const coolmnsTitles = columns.map(elem => elem.title)
+    const coolmnsTitles = columns.map(elem => ({title:elem.title,inputRender:elem.inputRender}))
 
    
-
-
     return (
         user === null ? <Navigate to='/login' /> : 
         <div>
