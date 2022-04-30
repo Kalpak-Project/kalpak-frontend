c82c7059a0af4db7ef694a00875ba669d2f6155a
PinhasZiv <63521561+PinhasZiv@users.noreply.github.com>
2022-04-30 15:40:57 +0300

    pull request from master to has (#8)
    * Alex roles status frontend (#5)
    
    * Adding a new routing of a placement meeting
    
    * Functionality of the Placement Meetings Table page
    
    Co-authored-by: alex94chen <alex94chen@gmail.com>
    
    * Alex staffing form page frontend (#7)
    
    * Adding a page of a staffing form
    
    * In StaffingForm adding space to detail relevant to the staffing of the position
    
    Co-authored-by: alex94chen <alex94chen@gmail.com>
    Co-authored-by: Alex Chen <69761913+alex94chen@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import UsersTable from './pages/UsersTable';
 import LogInPage from './pages/LogInPage';
 import ManningTable from './pages/ManningTable';
 import PlacementMeetingsTable from './pages/PlacementMeetingsTable';
+import StaffingForm from './pages/StaffingForm';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate  } from 'react-router-dom';
 import { UserContext, userData } from './components/userContext';
@@ -57,24 +58,30 @@ const App = () => {
           <Link to='/placementMeetings'>
           PlacementMeetings
           </Link>
+        </Menu.Item>
+        <Menu.Item  key="5">
+        <Link to='/staffingForm'>
+          StaffingForm
+          </Link>
         </Menu.Item></>
          }
         
         {user['isAdmin'] ?
         <SubMenu
-          key="5"
+          key="6"
           title={
             <span className="submenu-title-wrapper">
               {user.user}
             </span>
           }
-          style={{backgroundColor: '#87CEFA', marginLeft: '54%'}}
+          style={{backgroundColor: '#87CEFA', marginLeft: '44%'}}
+
         >
             <Menu.Item key="settings">Settings</Menu.Item>
             <Menu.Item onClick={() => logoutUser()} key="logout">Log out</Menu.Item>
         </SubMenu> : 
         <SubMenu
-        key="5"
+        key="6"
         title={
           <span className="submenu-title-wrapper">
             {user.user}
@@ -102,6 +109,7 @@ const App = () => {
         <Route path='/users' element={<UsersTable />} />
         <Route path='/manning' element={<ManningTable />} /> 
         <Route path='/placementMeetings' element={<PlacementMeetingsTable />} /> 
+        <Route path='/staffingForm' element={<StaffingForm />} /> 
        </Routes>
              
       </div>
